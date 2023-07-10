e933ec9d13ccf6308f08f8737fb72fad1da9a3ee
bakseck <seck.bakar@ugb.edu.sn>
2023-07-10 10:50:34 +0000

    rm...

---
+++ bakar/quest01/more-or-less.js
@@ -0,0 +1,17 @@
+// More Or Less
+
+function more(number) {
+    return number += 1
+}
+
+function less(number) {
+    return number -= 1
+}
+
+function add(number1, number2) {
+    return number1 + number2
+}
+
+function add(number1, number2) {
+    return number2 - number1
+}
