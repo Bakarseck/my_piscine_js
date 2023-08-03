43f176aa1385bfb85018e0ec6705f25cbf5a6fc6
bakseck <seck.bakar@ugb.edu.sn>
2023-08-03 17:58:28 +0000

    last_version...

--- bakar/quest01/dog-years.js
+++ bakar/quest01/dog-years.js
@@ -1,7 +1,29 @@
-const dogYears = (planetName, age) => {
-    if (planetName === "earth") {
-        
-    }
+function dogYears(planet, ageInSeconds) {
+
+    let orbitalPeriods = {
+        earth: 1.0,
+        mercury: 0.2408467,
+        venus: 0.61519726,
+        mars: 1.8808158,
+        jupiter: 11.862615,
+        saturn: 29.447498,
+        uranus: 84.016846,
+        neptune: 164.79132
+    };
+
+    let secondInEarthYears = 31557600
+
+    let secondInYears = secondInEarthYears * orbitalPeriods[planet]
+
+    let age = conversion(ageInSeconds, secondInYears)
+
+    let realAge = age*7
+
+    console.log(parseFloat(realAge.toFixed(2)))
 }
 
-console.log("earth", 1000000000)
+function conversion(ageInSeconds, secondInYears) {
+    return ageInSeconds / secondInYears
+}
+
+dogYears("earth", 1000000000)
